2b1728eaea2c34bddce7329ced668be781c09582
Miro <miro@miro-wilms.de>
2021-06-13 13:19:44 +0200

    initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/index.js", "./src/style.css"],
+  output: {
+    filename: "[name].js", // filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, "dist/app/"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }],
+              ["@babel/preset-react", { development: true }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
