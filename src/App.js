e80067c2d7f2a070e71b0cf18bd603bfc8165c5e
Miro <miro@miro-wilms.de>
2021-06-17 17:52:19 +0200

    added edit mode & handleChange function

--- src/App.js
+++ src/App.js
@@ -5,66 +5,104 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
 
+    // TODO: strip all strings from state again
+    // state is not meant to be used in such a nested way - updating a big
+    // part of the state on every keystroke is bad for performance
     this.state = {
       general: {
-        name: {
-          fieldName: "Name",
-          value: "",
-        },
-        email: {
-          fieldName: "E-Mail",
-          value: "",
-        },
-        phone: {
-          fieldName: "Phone number",
-          value: "",
-        },
+        title: "General",
+        fields: {
+          name: {
+            fieldName: "Name",
+            value: "",
+          },
+          email: {
+            fieldName: "E-Mail",
+            value: "",
+          },
+          phone: {
+            fieldName: "Phone number",
+            value: "",
+          },
+        }
       },
       education: {
-        school: {
-          fieldName: "School name",
-          value: "",
-        },
-        field: {
-          fieldName: "Field of studies",
-          value: "",
-        },
-        date: {
-          fieldName: "Date of graduation",
-          value: "",
-        },
+        title: "Education",
+        fields: {
+          school: {
+            fieldName: "School name",
+            value: "",
+          },
+          field: {
+            fieldName: "Field of studies",
+            value: "",
+          },
+          date: {
+            fieldName: "Date of graduation",
+            value: "",
+          },
+        }
       },
       practical: {
-        company: {
-          fieldName: "Company name",
-          value: "",
-        },
-        position: {
-          fieldName: "Title of position",
-          value: "",
-        },
-        task: {
-          fieldName: "Main task",
-          value: "",
-        },
-        dateFrom: {
-          fieldName: "From",
-          value: "",
-        },
-        dateUntil: {
-          fieldName: "Until",
-          value: "",
-        },
+        title: "Practical experience",
+        fields: {
+          company: {
+            fieldName: "Company name",
+            value: "",
+          },
+          position: {
+            fieldName: "Title of position",
+            value: "",
+          },
+          task: {
+            fieldName: "Main task",
+            value: "",
+          },
+          dateFrom: {
+            fieldName: "From",
+            value: "",
+          },
+          dateUntil: {
+            fieldName: "Until",
+            value: "",
+          },
+        }
       }
     }
+
+    this.handleChange = this.handleChange.bind(this);
+
   }
+
+  handleChange(e,area) {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    const prevState = this.state[area];
+    prevState["fields"][name]["value"] = value;
+
+    // console.log("NEW: " + area + "." + name + ": " + value);
+    // console.log("OLD: " + this.state[area]["fields"][name]["value"])
+
+    this.setState({
+      [area]: prevState,
+    })
+
+  }
+
   render() {
     return (
       <div id="App">
         <h1>CV App</h1>
-        <ShowInfo title="General" fields={this.state.general}/>
-        <ShowInfo title="Education" fields={this.state.education}/>
-        <ShowInfo title="Practical experience" fields={this.state.practical}/>
+        {Object.keys(this.state).map((area) =>
+          <ShowInfo
+            key={area}
+            area={area}
+            title={this.state[area].title}
+            fields={this.state[area].fields}
+            handleChange={this.handleChange} />
+        )}
       </div>
     )
 }
--- src/components/showInfo.js
+++ src/components/showInfo.js
@@ -2,17 +2,56 @@
 import React from 'react';
 
 export default class ShowInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: false,
+    }
+
+    this.setEditMode = this.setEditMode.bind(this);
+  }
+
+  setEditMode(mode) {
+    this.setState(() => ({
+      editMode: mode,
+    }))
+  }
+
   render() {
     const fields = this.props.fields;
-    return (
-      <div id="ShowInfo">
-        <h2>{this.props.title}</h2>
-        {Object.keys(fields).map((field) =>
-          <div className="field" key={fields[field].fieldName}>
-            {fields[field].fieldName}: {fields[field].value}
-          </div>
-        )}
-      </div>
-    )
+
+    if (!this.state.editMode) {
+      return (
+        <div className="ShowInfo">
+          <h2>{this.props.title}</h2>
+          {Object.keys(fields).map((field) =>
+            <div className="field" key={fields[field].fieldName}>
+              {fields[field].fieldName}: {fields[field].value}
+            </div>
+          )}
+          <button className="button" onClick={() => {this.setEditMode(true)}}>edit</button>
+        </div>
+      )
+    } else {
+      return (
+        <div className="ShowInfo">
+          <h2>{this.props.title}</h2>
+            {Object.keys(fields).map((field) =>
+              <div className="field" key={fields[field].fieldName}>
+                {fields[field].fieldName}: <input
+                  type="text"
+                  key={fields[field].fieldName}
+                  name={field}
+                  value={fields[field].value}
+                  onChange={(e) => this.props.handleChange(e,this.props.area)} />
+              </div>
+            )}
+            <button className="button" onClick={() => {this.setEditMode(false)}}>save</button>
+        </div>
+      )
+    }
+  }
 }
-}
+
+// TODO: extract view/edit renders in separate components?
